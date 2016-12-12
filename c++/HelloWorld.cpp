35256c1ceefc19d47e102d864dbb52b79501aada
Liam Lei <775138281@qq.com>
2016-12-12 15:34:55 +0800

    updates

---
+++ c++/HelloWorld.cpp
@@ -0,0 +1,10 @@
+//
+//  HelloWorld.cpp
+//  
+//
+//  Created by 雷昊 on 06/12/2016.
+//
+//
+
+#include "HelloWorld.hpp"
+
---
+++ c++/HelloWorld.hpp
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.hpp
+//  
+//
+//  Created by 雷昊 on 06/12/2016.
+//
+//
+
+#ifndef HelloWorld_hpp
+#define HelloWorld_hpp
+
+#include <stdio.h>
+
+#endif /* HelloWorld_hpp */

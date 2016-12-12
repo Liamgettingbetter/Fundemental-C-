e48d077b0ed2a87b9ceceaea22b26ed9f194144d
Liam Lei <775138281@qq.com>
2016-12-12 22:07:57 +0800

    updates

---
+++ Cpp/Charlist.cpp
@@ -0,0 +1,14 @@
+//: C03: Charlist.cpp
+// Display all the ASCII characters
+// Demonstrates "for"
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(int i = 0; i < 128; i++)
+		if(i != 26) 	// ANSI Terminal Clear screen
+			cout << " value: " << i
+				 << " character: " 
+				 << char(i) // Type conversion
+				 << endl;
+}

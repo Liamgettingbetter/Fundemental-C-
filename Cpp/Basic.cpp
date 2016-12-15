ddefd770ff31b5a5adfcd48af8f79caa9065247b
Liam Lei <775138281@qq.com>
2016-12-15 14:56:10 +0800

    updates

---
+++ Cpp/Basic.cpp
@@ -0,0 +1,20 @@
+//: C03: Basic.cpp
+// Defining the four basic data
+// types in C and C++
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Definition without initialization;
+	char protection;
+	int carbohydrates;
+	float fiber;
+	double fat;
+	// Simultaneous definition & initialization
+	char pizza = 'A', pop = 'Z';
+	int dongdings = 100, twinkles = 150,
+		heehos = 200;
+	float chocolate = 3.14159;
+	// Exponential notation 
+	double fudge_ripple = 6e-4;
+}

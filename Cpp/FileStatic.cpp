b2f6a1f906383aa96970d43e994438c20438927d
Liam Lei <775138281@qq.com>
2016-12-16 14:46:57 +0800

    updates

---
+++ Cpp/FileStatic.cpp
@@ -0,0 +1,6 @@
+//: C03 : FileStatic.cpp
+// File scope demonstration. Compiling and linking this file 
+// with FileStatic2.cpp 
+// will cause a linker error
+
+// File scope means only available in this file
---
+++ Cpp/FileStatic2.cpp
@@ -0,0 +1,7 @@
+//: C03: FileStatic2.cpp
+// Trying to reference fs
+// extern int fs;	
+
+void func() {
+	//fs = 100;
+}

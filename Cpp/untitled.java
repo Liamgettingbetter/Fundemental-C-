0738ebf181768c1e40a2e81181458984b21f76e4
Liam Lei <775138281@qq.com>
2016-12-16 14:50:24 +0800

    updates

--- Cpp/untitled.java
+++
@@ -1,9 +0,0 @@
-public class Liam {
-	public static void main(String[] args) {
-		Pattern p = Pattern.compile("a*b");		// matches all of strings that has 'a' as prefix and 'b' as suffix
-		Matcher m = p.matches("aaaaab");		// "aaaaab" matches "a*b"
-		boolean b = m.matches();				// b is true
-		
-		boolean b = Pattern.matches("a*b", "aaaaab");	// equivalent to the three statements above
-	}
-}

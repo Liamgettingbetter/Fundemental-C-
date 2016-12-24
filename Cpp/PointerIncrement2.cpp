59b22b1c49db7433c2b12d5afcdafd870fd32375
Liam Lei <775138281@qq.com>
2016-12-24 18:42:42 +0800

    updates

---
+++ Cpp/PointerIncrement2.cpp
@@ -0,0 +1,24 @@
+//: C03:PointerIncrement2.cpp
+#include <iostream>
+using namespace std;
+
+typedef struct {
+	char c;
+	short s;
+	int i;
+	long l;
+	float f;
+	double d;
+	long double ld;
+} Primitives;
+
+int main() {
+	Primitives p[10];
+	primitives *pp = p;
+
+	cout << "sizeof(Primitives) = "
+		 << sizeof(Primitives) << endl;
+	cout << "pp = " << (long)pp << endl;
+	pp++;
+	cout << "pp = " << (long)pp << endl;
+}

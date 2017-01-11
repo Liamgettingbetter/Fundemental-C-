f9b70ca09dab1c9fe8315a80df84e5b4c3ac0119
Liam Lei <775138281@qq.com>
2017-01-11 21:50:44 +0800

    updates

--- Cpp/chapter3/HelloWorld.cpp
+++ Cpp/chapter3/HelloWorld.cpp
@@ -1,20 +1,7 @@
-//: C02 : Declare.cpp
-// Declaration & definition examples
-extern int i;			// Declaration without definition
-extern float f(float); 	// function declaration
-float b;				// Declaration & definition
-float f(float a) {		// Definition
-	return a + 1.0;
-}
-
-int i;					// Definition
-int h(int x) {			// Declaration & definition
-	return x + 1;
-}
+// Hello World
+#include <iostream>
 
 int main() {
-	b = 1.0;
-	i = 2;
-	f(b);
-	h(i);
+	std::cout << "Hello World !" << std::endl;
+	return 0;
 }

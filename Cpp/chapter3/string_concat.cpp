b76ef1b584f2d00e831664274b8762d882e6de94
Liam Lei <775138281@qq.com>
2017-01-28 11:32:57 +0800

    updates

--- Cpp/chapter3/string_concat.cpp
+++ Cpp/chapter3/string_concat.cpp
@@ -6,7 +6,7 @@ int main() {
 	const char a[] = "A Lannister always";
 	const char b[] = "pays his debts.";
 
-	char c[strlen(a) + strlen(b)];
+	char c[strlen(a) + strlen(b) + 2];
 	strcpy(c, a);
 	strcat(c, " ");
 	strcat(c, b);
